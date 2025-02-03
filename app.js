a801d2a160ccd42aa60310930bd0e936d49d2ada
Amandeep-Nugen <amandeep.nugen@gmail.com>
2025-02-03 11:28:23 +0530

    modules.exports

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const data = require("./data.json");
 // require('./sum')
 // const calculateSum=require('./sum')
 // const obj=require('./sum.js')
-const {x,calculateSum}=require('./sum.js')
+// const {calculateSum}=require('./calculate')
 // you cannot access variables and methods functions of one module into another by the used of require
 //modules protects their variables and functions from leaking
 // globalThis introduced in ES2020 now called the global variable
@@ -30,11 +30,19 @@ calculateMultiply(a, b);
 // console.log(data);
 
 
+
+
+
+
+
 var name="Amandeep";
 var a=10;
 var b=20;
 // obj.calculateSum(a,b);
 calculateSum(a,b)
-console.log(x)
+// console.log(x)
 // console.log(obj.x)
- console.log(globalThis===global)
+console.log(data
+
+)
+console.log(globalThis===global)
